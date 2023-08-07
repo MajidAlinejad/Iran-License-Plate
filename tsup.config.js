41158b4d91ec2fa5de1cef21bad57f260d68d761
majid <alinejad.mj@gmail.com>
2023-08-07 21:08:30 +0330

    update .npmrc, package.json, tsconfig.json and tsup.config.js

--- tsup.config.js
+++ tsup.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/IranLicensePlate"],
+
   splitting: false,
   sourcemap: true,
   clean: true,
+  legacyOutput: true,
+  // onSuccess: "tsc --emitDeclarationOnly --declaration",
   loader: {
     ".jpg": "base64",
     ".css": "css",
